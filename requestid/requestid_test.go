0aae3c6d2c687644ff5c2361ce4bf656c28ac5c5
davidsbond <davidsbond93@gmail.com>
2020-09-21 21:50:56 +0100

    Move request id logic out of middleware
    The `requestid` package was somewhat useless on its own, this moves the main logic
    for generating/propagating request identifiers into a requestid.Extract method and
    simplifies the middleware.

---
+++ requestid/requestid_test.go
@@ -0,0 +1,44 @@
+package requestid_test
+
+import (
+	"context"
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"pkg.dsb.dev/requestid"
+)
+
+func TestExtract(t *testing.T) {
+	t.Parallel()
+	const key = "X-Request-ID"
+
+	tt := []struct {
+		Name          string
+		RequestHeader http.Header
+	}{
+		{
+			Name: "It should use an existing request id",
+			RequestHeader: map[string][]string{
+				key: {"test-id"},
+			},
+		},
+		{
+			Name:          "It should generate an identifier",
+			RequestHeader: make(http.Header),
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.Name, func(t *testing.T) {
+			ctx := context.Background()
+			respHeader := make(http.Header)
+			ctx = requestid.Extract(ctx, tc.RequestHeader, respHeader)
+
+			assert.NotEmpty(t, tc.RequestHeader.Get(key))
+			assert.NotEmpty(t, respHeader.Get(key))
+			assert.EqualValues(t, tc.RequestHeader.Get(key), respHeader.Get(key))
+		})
+	}
+}
