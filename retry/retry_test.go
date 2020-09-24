3d47fcccd86dc6c7fff7d1acaf7c47f8d55ffa4c
David Bond <davidsbond@users.noreply.github.com>
2020-09-24 18:02:06 +0100

    pkg/retry: Create retry package (#15)
    Adds a retry package for performing functions multiple times if they return
    an error. Updates the cron package to always try up to 3 times before
    returning.

---
+++ retry/retry_test.go
@@ -0,0 +1,35 @@
+package retry_test
+
+import (
+	"context"
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"pkg.dsb.dev/retry"
+)
+
+func TestDo(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+
+	// Test no error only invokes once
+	invoked := 0
+	assert.NoError(t, retry.Do(ctx, 3, func(_ context.Context) error {
+		invoked++
+		return nil
+	}))
+
+	assert.EqualValues(t, 1, invoked)
+
+	// Test error invokes the maximum times before returning
+	invoked = 0
+	assert.Error(t, retry.Do(ctx, 3, func(_ context.Context) error {
+		invoked++
+		return io.EOF
+	}))
+
+	assert.EqualValues(t, 3, invoked)
+}
