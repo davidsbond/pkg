21ea26fc22babc1823368662f802e91b287241a2
davidsbond <davidsbond93@gmail.com>
2020-10-04 23:27:12 +0100

    pkg/server: Add bootstrapping for a gRPC server
    Adds `server.ServeGRPC` which starts a new gRPC server listening on port 5000 that has
    health checks & instrumentation for tracing, metrics, panic recovery, request validation
    and context tag extration.
    
    Creates a `GRPCService` abstraction for types that implement a gRPC service interface. They must
    include a `Register` method that registers the type to the `grpc.Server` instance. This keeps the
    specifics of the gRPC code close to the implementation.

--- server/http_options.go
+++ server/http_options.go
@@ -10,19 +10,19 @@ import (
 type (
 	// The HTTPOption type represents a server option that can be used to modify
 	// the HTTP server configuration.
-	HTTPOption func(c *config)
+	HTTPOption func(c *httpConfig)
 )
 
 // WithHandler sets the server's handler to the one provided.
 func WithHandler(r *mux.Router) HTTPOption {
-	return func(c *config) {
+	return func(c *httpConfig) {
 		c.handler = r
 	}
 }
 
 // WithSecurity enables/disables the server's SSL features.
 func WithSecurity(flag bool) HTTPOption {
-	return func(c *config) {
+	return func(c *httpConfig) {
 		c.securityEnabled = flag
 	}
 }
@@ -30,7 +30,7 @@ func WithSecurity(flag bool) HTTPOption {
 // WithTLSCache determines the location on the filesystem that TLS certificates
 // will be cached.
 func WithTLSCache(dir string) HTTPOption {
-	return func(c *config) {
+	return func(c *httpConfig) {
 		c.tlsCache = dir
 	}
 }
@@ -41,7 +41,7 @@ var ErrInvalidTLSHost = errors.New("invalid tls host")
 
 // WithAllowedHosts sets hosts that are allowed to make TLS requests.
 func WithAllowedHosts(hosts ...string) HTTPOption {
-	return func(c *config) {
+	return func(c *httpConfig) {
 		c.tlsHostPolicy = func(ctx context.Context, host string) error {
 			for _, h := range hosts {
 				if h == host {
